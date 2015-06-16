d7eeff9eefb5e52cfc8a7adb143a8e9b8b562006
Alexander Birk <birk@pingworks.de>
2015-06-16 09:07:04 +0200

    simplistic binrepo lister

--- lib/mofa/binrepo_list_cmd.rb
+++ lib/mofa/binrepo_list_cmd.rb
@@ -18,20 +18,20 @@ class BinrepoListCmd < MofaCmd
     puts "Will use ssh_user #{Mofa::Config.config['binrepo_ssh_user']} and ssh_key_file #{Mofa::Config.config['binrepo_ssh_keyfile']}"
 
     begin
-
       Net::SSH.start(Mofa::Config.config['binrepo_host'],
                      Mofa::Config.config['binrepo_ssh_user'],
                      keys: [Mofa::Config.config['binrepo_ssh_keyfile']],
                      port: Mofa::Config.config['binrepo_ssh_port'],
                      verbose: :error,
                      use_agent: false) do |ssh|
-        out = ssh_exec!(ssh, "ls -al")
+        out = ssh_exec!(ssh, "tree -n -d -L 2 #{Mofa::Config.config['binrepo_import_dir']} | grep -v directories")
+        puts out[1]
         fail "ERROR (#{out[0]}): #{out[2]}" if out[0] != 0
       end
 
     rescue RuntimeError => e
       puts "Error: #{e.message}"
-      raise "Failed to list binrepo content!"
+      raise 'Failed to list binrepo content!'
     end
   end
 end
