ae055c63d4185f612bc49d4f32b8462a5396309d
Christoph Lukas <lukas@pingworks.de>
2015-10-26 17:33:46 +0100

    refactored provision cmd to support released cookbooks

--- lib/mofa/provision_cmd.rb
+++ lib/mofa/provision_cmd.rb
@@ -42,8 +42,93 @@ class ProvisionCmd < MofaCmd
   # This Code is Copy'n'Pasted from the old mofa tooling. Only to make the MVP work in time!!
   # This needs to be refactored ASAP.
 
+  def prepare_host(hostname, host_index, solo_dir)
+    prerequesits_met = false
+    puts
+    puts "----------------------------------------------------------------------"
+    puts "Chef-Solo on Host #{hostname} (#{host_index}/#{hostlist.list.length.to_s})"
+    puts "----------------------------------------------------------------------"
+
+    puts "Pinging host #{hostname}..."
+    exit_status = system("ping -q -c 1 #{hostname} >/dev/null 2>&1")
+    unless exit_status then
+      puts "  --> Host #{hostname} is unavailable!"
+      chef_solo_runs[hostname].store('status', 'UNAVAIL')
+      chef_solo_runs[hostname].store('status_msg', "Host #{hostname} unreachable.")
+    else
+      puts "  --> Host #{hostname} is available."
+      prerequesits_met = true
+      Net::SSH.start(hostname, Mofa::Config.config['ssh_user'], :keys => [Mofa::Config.config['ssh_keyfile']], :port => Mofa::Config.config['ssh_port'], :verbose => :error) do |ssh|
+        puts "Remotely creating solo_dir \"#{solo_dir}\" on host #{hostname}"
+        # remotely create the temp folder
+        out = ssh_exec!(ssh, "[ -d #{solo_dir} ] || mkdir #{solo_dir}")
+        puts "ERROR (#{out[0]}): #{out[2]}" if out[0] != 0
+
+        # remotely create a data_bags folder structure on the target host
+        if cookbook.instance_of?(SourceCookbook) and File.directory?("#{cookbook.source_dir}/data_bags")
+          Dir.entries("#{cookbook.source_dir}/data_bags").select { |f| !f.match(/^\.\.?$/) }.each do |data_bag|
+            puts "Remotely creating data_bags dir \"#{solo_dir}/data_bags/#{data_bag}\""
+            out = ssh_exec!(ssh, "[ -d #{solo_dir}/data_bags/#{data_bag} ] || mkdir -p #{solo_dir}/data_bags/#{data_bag}")
+            puts "ERROR (#{out[0]}): #{out[2]}" if out[0] != 0
+          end
+        end
+      end
+    end
+    prerequesits_met
+  end
+
+  def create_solo_rb(sftp, hostname, solo_dir)
+    puts "Remotely creating \"#{solo_dir}/solo.rb\""
+    sftp.file.open("#{solo_dir}/solo.rb", "w") do |file|
+      if cookbook.instance_of?(SourceCookbook)
+        solo_rb = <<-"EOF"
+    cookbook_path [ "#{solo_dir}/cookbooks" ]
+        EOF
+      else
+        solo_rb = <<-"EOF"
+    recipe_url "#{cookbook.cookbooks_url}"
+        EOF
+      end
+      solo_rb += <<-"EOF"
+    data_bag_path "#{solo_dir}/data_bags"
+    log_level :info
+    log_location "#{solo_dir}/log"
+    verify_api_cert true
+      EOF
+
+      file.write(solo_rb)
+    end
+  end
+
+  def create_node_json(sftp, hostname, solo_dir, attributes_map)
+    puts "Remotely creating \"#{solo_dir}/node.json\""
+    node_json = {}
+    node_json.store('run_list', runlist_map.mp[hostname])
+    attributes_map.mp[hostname].each do |key, value|
+      node_json.store(key, value)
+    end
+
+    sftp.file.open("#{solo_dir}/node.json", "w") do |file|
+      file.write(JSON.pretty_generate(node_json))
+    end
+  end
+
+  def create_data_bags(sftp, hostname, solo_dir)
+    if cookbook.instance_of?(SourceCookbook) and File.directory?("#{cookbook.source_dir}/data_bags")
+      Dir.entries("#{cookbook.source_dir}/data_bags").select { |f| File.directory?(f) && !f.match(/^\.\.?$/) }.each do |data_bag|
+        Dir.entries("#{cookbook.source_dir}/data_bags/#{data_bag}").select { |f| f.match(/\.json$/) }.each do |data_bag_item|
+          puts "Uploading data_bag_item #{data_bag_item}... "
+          sftp.upload!("#{cookbook.source_dir}/data_bags/#{data_bag}/#{data_bag_item}", "#{solo_dir}/data_bags/#{data_bag}/#{data_bag_item}")
+          puts "OK."
+        end
+      end
+    end
+  end
+
   def run_chef_solo_on_hosts
     time = Time.new
+    # Create a temp working dir on the target host
+    solo_dir = '/var/tmp/' + time.strftime('%Y-%m-%d_%H%M%S')
     puts
     puts 'Chef-Solo Run started at ' + time.strftime('%Y-%m-%d %H:%M:%S')
     puts "Will use ssh_user #{Mofa::Config.config['ssh_user']}, ssh_port #{Mofa::Config.config['ssh_port']} and ssh_key_file #{Mofa::Config.config['ssh_keyfile']}"
@@ -52,82 +137,19 @@ class ProvisionCmd < MofaCmd
     host_index = 0
     hostlist.list.each do |hostname|
       host_index = host_index + 1
-      puts
-      puts "----------------------------------------------------------------------"
-      puts "Chef-Solo on Host #{hostname} (#{host_index}/#{hostlist.list.length.to_s})"
-      puts "----------------------------------------------------------------------"
+      next unless prepare_host(hostname, host_index, solo_dir)
       chef_solo_runs.store(hostname, {})
-
-      puts "Pinging host #{hostname}..."
-      exit_status = system("ping -q -c 1 #{hostname} >/dev/null 2>&1")
-      unless exit_status then
-        puts "  --> Host #{hostname} is unavailable!"
-        chef_solo_runs[hostname].store('status', 'UNAVAIL')
-        chef_solo_runs[hostname].store('status_msg', "Host #{hostname} unreachable.")
-      else
-        puts "  --> Host #{hostname} is available."
-        prerequesits_met = true
-        # Create a temp working dir on the target host
-        solo_dir = '/var/tmp/' + time.strftime('%Y-%m-%d_%H%M%S')
-        Net::SSH.start(hostname, Mofa::Config.config['ssh_user'], :keys => [Mofa::Config.config['ssh_keyfile']], :port => Mofa::Config.config['ssh_port'], :verbose => :error) do |ssh|
-          puts "Remotely creating solo_dir \"#{solo_dir}\" on host #{hostname}"
-          # remotely create the temp folder
-          out = ssh_exec!(ssh, "[ -d #{solo_dir} ] || mkdir #{solo_dir}")
-          puts "ERROR (#{out[0]}): #{out[2]}" if out[0] != 0
-
-          # remotely create a data_bags folder structure on the target host
-          if File.directory?("#{cookbook.source_dir}/data_bags")
-            Dir.entries("#{cookbook.source_dir}/data_bags").select { |f| !f.match(/^\.\.?$/) }.each do |data_bag|
-              puts "Remotely creating data_bags dir \"#{solo_dir}/data_bags/#{data_bag}\""
-              out = ssh_exec!(ssh, "[ -d #{solo_dir}/data_bags/#{data_bag} ] || mkdir -p #{solo_dir}/data_bags/#{data_bag}")
-              puts "ERROR (#{out[0]}): #{out[2]}" if out[0] != 0
-            end
-          end
-        end
-      end
-
-      # skip the rest if prerequesits are not met
-      next unless prerequesits_met
-
 
       Net::SFTP.start(hostname, Mofa::Config.config['ssh_user'], :keys => [Mofa::Config.config['ssh_keyfile']], :port =>  Mofa::Config.config['ssh_port'], :verbose => :error) do |sftp|
 
         # remotely creating solo.rb
-        puts "Remotely creating \"#{solo_dir}/solo.rb\""
-        sftp.file.open("#{solo_dir}/solo.rb", "w") do |file|
-          solo_rb = <<-"EOF"
-        cookbook_path [ "#{solo_dir}/cookbooks" ]
-        data_bag_path "#{solo_dir}/data_bags"
-        log_level :info
-        log_location "#{solo_dir}/log"
-        verify_api_cert true
-          EOF
-
-          file.write(solo_rb)
-        end
+        create_solo_rb(sftp, hostname, solo_dir)
 
         # remotely creating node.json
-        puts "Remotely creating \"#{solo_dir}/node.json\""
-        node_json = {}
-        node_json.store('run_list', runlist_map.mp[hostname])
-        attributes_map.mp[hostname].each do |key, value|
-          node_json.store(key, value)
-        end
-
-        sftp.file.open("#{solo_dir}/node.json", "w") do |file|
-          file.write(JSON.pretty_generate(node_json))
-        end
+        create_node_json(sftp, hostname, solo_dir, attributes_map)
 
         # remotely create data_bag items
-        if File.directory?("#{cookbook.source_dir}/data_bags")
-          Dir.entries("#{cookbook.source_dir}/data_bags").select { |f| File.directory?(f) && !f.match(/^\.\.?$/) }.each do |data_bag|
-            Dir.entries("#{cookbook.source_dir}/data_bags/#{data_bag}").select { |f| f.match(/\.json$/) }.each do |data_bag_item|
-              puts "Uploading data_bag_item #{data_bag_item}... "
-              sftp.upload!("#{cookbook.source_dir}/data_bags/#{data_bag}/#{data_bag_item}", "#{solo_dir}/data_bags/#{data_bag}/#{data_bag_item}")
-              puts "OK."
-            end
-          end
-        end
+        create_data_bags(sftp, hostname, solo_dir)
 
         if cookbook.instance_of?(SourceCookbook)
           puts "Cookbook is a SourceCookbook! Uploading Snapshot Package #{cookbook.pkg_name}... "
